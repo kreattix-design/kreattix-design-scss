7d03327478089407a643e662233fa9a688ea1a00
Prince Patel <ext_prpatel@falabella.cl>
2022-11-07 14:28:28 +0530

    base setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import EsLint from 'vite-plugin-linter'
+import tsConfigPaths from 'vite-tsconfig-paths'
+const { EsLinter, linterPlugin } = EsLint
+import { resolve } from 'node:path'
+
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ['./src}/**/*.{ts,tsx}'],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ['src/component/'],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve('src', 'index.ts'),
+      name: 'kreattix-design-icons',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+}))
