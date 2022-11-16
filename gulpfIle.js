a19b7d1f739658fef7b60516b319f322956b0623
Prince Patel <ext_prpatel@falabella.cl>
2022-11-16 18:22:08 +0530

    setup

---
+++ gulpfIle.js
@@ -0,0 +1,30 @@
+const GulpClient = require('gulp')
+const GulpCleanCss = require('gulp-clean-css')
+const GulpRename = require('gulp-rename')
+const GulpSass = require('gulp-sass')(require('sass'))
+const GulpUtil = require('gulp-util')
+const GulpSourceMaps = require('gulp-sourcemaps')
+
+const compileScss = () => {
+  return GulpClient.src('./scss/**/*.{sass,scss}')
+    .pipe(GulpSourceMaps.init())
+    .pipe(GulpSass())
+    .on('error', GulpUtil.log.bind(GulpUtil, 'Sass Error'))
+    .pipe(GulpSourceMaps.write())
+    .pipe(GulpClient.dest('./dist/css/'))
+    .pipe(
+      GulpRename(function (file) {
+        file.basename = file.basename + '.min'
+      }),
+    )
+    .pipe(GulpCleanCss())
+    .pipe(GulpSourceMaps.write())
+    .pipe(GulpClient.dest('./dist/css/'))
+}
+
+const watchScss = () => {
+  return GulpClient.watch('./scss/**/*.{sass,scss}', compileScss)
+}
+
+exports.compileScss = compileScss
+exports.watchScss = watchScss
